3d024059a3d8d13bd83ff8c7d01fceb72e941d92
Yassine Borak <yassineborak@gmail.com>
2025-10-16 02:59:51 +0100

    Added Generating pwd funcs

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordEl = document.getElementById("password")
+let passwordEl2 = document.getElementById("password2")
+let generateBtn = document.getElementById("generate")
 
+function randomPwd() {
+    return characters[Math.floor(Math.random()*characters.length)]
+}
+
+function getPwd(length=14) {
+    let p1 = ""
+    let p2 = ""
+    for (i=0; i<length; i++) {
+        p1+=randomPwd()
+        p2+=randomPwd()
+    }
+    passwordEl.value = p1
+    passwordEl2.value = p2
+}
+
+function generatePasswords() {
+    getPwd()
+}
